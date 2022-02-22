cdc573c175ce1477cbc3afda1987d032712513f6
laurpe <laura.perttila@edu.bc.fi>
2022-02-22 09:10:14 +0200

    css for buttons, inputs, layout

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 const bill = document.querySelector("#bill");
 
-bill.addEventListener("change", (event) => {
+bill.addEventListener("keyup", (event) => {
     console.log(event.target.value);
 });
-
-//tee radiobuttonit, silloin ei tarvitse seurata erikseen joka ikisen nappulan painamista
 
 const numberOfPeople = document.querySelector("#number-of-people");
 
-numberOfPeople.addEventListener("change", (event) => {
+numberOfPeople.addEventListener("keyup", (event) => {
     console.log(event.target.value);
 });
+
+const tipSelection = document.getElementsByClassName(".tip-selection");
+
+console.log(tipSelection);
